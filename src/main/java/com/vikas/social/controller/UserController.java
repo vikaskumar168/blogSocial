12ec8a117aa6d8f7b7b3d4a3bdccce388c8bd8b1
vikas <vikaskmr2411@gmail.com>
2026-01-27 23:22:09 +0530

    Added some code.

--- src/main/java/com/vikas/social/controller/UserController.java
+++ src/main/java/com/vikas/social/controller/UserController.java
@@ -2,6 +2,8 @@ package com.vikas.social.controller;
 
 import com.vikas.social.pojos.RegisterUserRequest;
 import com.vikas.social.pojos.RegisterUserResponse;
+import com.vikas.social.pojos.UserLoginRequest;
+import com.vikas.social.pojos.UserLoginResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +15,10 @@ public class UserController {
     @PostMapping("/signup")
     public RegisterUserResponse registerUser(RegisterUserRequest registerUserRequest){
         return RegisterUserResponse.builder().build();
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponse loginUser(UserLoginRequest userLoginRequest){
+        return UserLoginResponse.builder().build();
     }
 }
---
+++ src/main/java/com/vikas/social/service/UserServiceImpl.java
@@ -0,0 +1,9 @@
+package com.vikas.social.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl {
+
+
+}
